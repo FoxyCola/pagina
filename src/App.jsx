59d41c4588488503d95408b7cecfdb4027d9461e
JA Velez <143342171+FoxyCola@users.noreply.github.com>
2025-06-05 07:18:03 -0500

    correccion de carpetas en appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Login from '../pages/Login';
-import Chat from '../pages/Chat';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 import './App.css'
 
 function App() {
